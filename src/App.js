7cf496f0cf73a68f12de6a3e4afe20bb5d075e74
maiphuonghoang <maiphuonghoangmpk@gmail.com>
2023-02-15 01:11:02 +0700

    Cấu hình Layout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
+import { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { publicRoutes } from '~/routes';
-
+import { DefaultLayout } from '~/components/Layout';
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Routes>
-                    {publicRoutes.map((route, index) => {
-                        const Page = route.component;
-                        return <Route key={index} path={route.path} element={<Page />} />;
-                    })}
-                </Routes>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            const Page = route.component;
+            // const Layout = route.layout === null ? Fragment : DefaultLayout;
+            let Layout = DefaultLayout;
+            if (route.layout) {
+              Layout = route.layout;
+            } else if (route.layout === null) {
+              Layout = Fragment;
+            }
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,23 @@
+//Layout
+import { HeaderOnly } from '~/components/Layout';
+
 import Home from '~/pages/Home';
 import Following from '~/pages/Following';
 import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
 
+//Public routes
 const publicRoutes = [
-    //không cần đăng nhập
+  //không cần đăng nhập
 
-    { path: '/', component: Home },
-    { path: '/following', component: Following },
-    { path: '/profile', component: Profile },
+  { path: '/', component: Home },
+  { path: '/following', component: Following },
+  { path: '/profile', component: Profile },
+  { path: '/upload', component: Upload, layout: HeaderOnly },
+  { path: '/search', component: Search, layout: null },
 ];
 const privateRoutes = [
-    //nếu chưa đăng nhập thì chuyển sang login
+  //nếu chưa đăng nhập thì chuyển sang login
 ];
 export { publicRoutes, privateRoutes };
