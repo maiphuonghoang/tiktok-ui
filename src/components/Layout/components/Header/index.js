f903aa93c813bcb79f23c88a26175c4c8a43ae2f
maiphuonghoang <maiphuonghoangmpk@gmail.com>
2023-02-15 11:14:59 +0700

    Xây dựng UI phần Header #1 Logo Tiktok

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,11 +1,17 @@
 import styles from './Header.module.scss';
 import classNames from 'classnames/bind';
+import images from '~/assets/images';
 const cx = classNames.bind(styles);
 //bind giúp trả ra 1 method giúp viết css dưới dạng dấu gạch ngang
+
 function Header() {
   return (
     <header className={cx('wrapper')}>
-      <div className={cx('inner')}></div>
+      <div className={cx('inner')}>
+        <div className={cx('logo')}>
+          <img src={images.logo} alt="Tiktok" />
+        </div>
+      </div>
     </header>
   );
 }
