641af6b9d7bb87e0bf0f0317c0fb2477c3421dce
maiphuonghoang <maiphuonghoangmpk@gmail.com>
2023-02-15 10:40:48 +0700

    Dựng khung Layout mặc định

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,13 @@
+import styles from './Header.module.scss';
+import classNames from 'classnames/bind';
+const cx = classNames.bind(styles);
+//bind giúp trả ra 1 method giúp viết css dưới dạng dấu gạch ngang
 function Header() {
-  return <h2>Header</h2>;
+  return (
+    <header className={cx('wrapper')}>
+      <div className={cx('inner')}></div>
+    </header>
+  );
 }
 
 export default Header;
